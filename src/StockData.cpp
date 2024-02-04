3602bd30f494211d679fd71baf05b47f138b5602
Jensa <jensavdl@gmail.com>
2024-02-04 20:04:47 +0100

    updating git ignore

--- src/StockData.cpp
+++ src/StockData.cpp
@@ -1,23 +1,6 @@
+#include "StockData.h"
+#include <iostream>
 
-
-
-
-
-
-
-    // kan ha en GUI , applikation som ritar grafer och jag får stega fram månad efter månad. 
-    // skicka data hit och hanterar det enligt strategier. osv.
-
-
-
-
-
-
-
-/*
-    Kan samköra aktierna mot vaaranda, med återinvesterande när det finns från försäljing från andra bolag. se hur det påverkar 
-    Fixa CSV för att generera liknande bid som jag har manuellt
-
-
-
-*/
+int HelloWorld() {
+    return 1;
+}
---
+++ src/StockData.h
@@ -0,0 +1,10 @@
+#ifndef STOCKDATA_H
+#define STOCKDATA_H
+#include <string>
+
+extern "C" __declspec(dllexport) int HelloWorld();
+
+
+
+#endif
+
